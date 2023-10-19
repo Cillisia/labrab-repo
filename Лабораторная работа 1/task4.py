c47fb27315d0f394cb4c643578426183507ac31a
Sofia Antipenko <85940833+Cillisia@users.noreply.github.com>
2023-10-19 23:03:11 +0300

    Update task4.py

--- Лабораторная работа 1/task4.py
+++ Лабораторная работа 1/task4.py
@@ -1,6 +1,11 @@
-# TODO Найдите количество книг, которое можно разместить на дискете
-import math
-size_of_one_in_bytes = 100*50*25*4
-memory_in_bytes = 1.44*1024*1024
-num_of_books = math.floor(memory_in_bytes/size_of_one_in_bytes)
-print("Количество книг, помещающихся на дискету:", num_of_books)
+users = ['user1', 'user2', 'user3', 'user1', 'user4', 'user2']
+dict_ = {
+    "Общее количество": 0,
+    "Уникальные посещения": 0
+}
+set_users = set(users)
+dict_["Общее количество"] = len(users)
+dict_["Уникальные посещения"] = len(set_users)
+print(dict_)
+# TODO Добавьте словарь и замените в нем нулевые значения статисчикой посещений
+
