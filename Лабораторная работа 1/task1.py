d19a88977e131d60e4fe0de6fa50b9e592e48659
Sofia Antipenko <85940833+Cillisia@users.noreply.github.com>
2023-10-19 23:00:01 +0300

    Update task1.py

--- Лабораторная работа 1/task1.py
+++ Лабораторная работа 1/task1.py
@@ -2,13 +2,12 @@ numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -
 
 # TODO заменить значение пропущенного элемента средним арифметическим
 l = len(numbers)
-sum = 0
+summ = 0
 for i in range(l):
     if numbers[i] is None:
         position = i
-    else:
-        sum+=numbers[i]
 
-average = sum/l
+summ = sum(numbers[:position])+ sum(numbers[position+1:])
+average = summ/l
 numbers[position]=average
 print("Измененный список:", numbers)
